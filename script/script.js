e5a271c3e1916cae2a048cdffbe9c4a77de84659
hutyra.beniamin <hutyra.beniamin@outlook.com>
2020-06-20 13:53:03 +0200

    add script folder and changes

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () { });
+{
+    var Location_1 = /** @class */ (function () {
+        function Location() {
+            this.name = "";
+        }
+        return Location;
+    }());
+    constructor(name, city, zipCode, address, img, created);
+    {
+        this.name = name;
+        this.city = city;
+        this.zipCode,
+            this.address = address;
+        this.img = created;
+    }
+}
---
+++ script/script.ts
@@ -0,0 +1,23 @@
+$(document).ready(function()){
+
+class Location {
+    name: string ="";
+    city: string;
+    zipCode: number;
+    address: string;
+    img: string;
+    created: string;
+}
+
+    constructor(name,city, zipCode, address, img, created){
+    this.name = name;
+    this.city = city;
+    this.zipCode,
+    this.address = address;
+    this.img = created; 
+}
+
+
+
+
+}
